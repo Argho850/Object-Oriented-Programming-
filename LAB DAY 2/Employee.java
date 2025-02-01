f84f4e7126bfbf24db75e61c4be4c310515c78d0
Argho850 <157719278+Argho850@users.noreply.github.com>
2025-02-01 23:52:29 +0600

    Update Employee.java

--- LAB DAY 2/Employee.java
+++ LAB DAY 2/Employee.java
@@ -1,2 +1,60 @@
-public class Employee {
+import java.util.Scanner;
+
+class employee {
+    private String name;
+    private int age;
+    private double salary;
+
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public static void main(String[] args) {
+        employee employee = new employee();
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter employee's name: ");
+        String name = scanner.nextLine();
+        employee.setName(name);
+
+        System.out.print("Enter employee's age: ");
+        int age = scanner.nextInt();
+        employee.setAge(age);
+
+        System.out.print("Enter employee's salary: ");
+        double salary = scanner.nextDouble();
+        employee.setSalary(salary);
+
+        
+        System.out.println("\nEmployee Details:");
+        System.out.println("Name: " + employee.getName());
+        System.out.println("Age: " + employee.getAge());
+        System.out.println("Salary: " + employee.getSalary());
+    }
 }
