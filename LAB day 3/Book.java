4001f030f8f26274503a2246b9810b7c029ab8ef
Argho850 <157719278+Argho850@users.noreply.github.com>
2025-02-12 20:52:07 +0600

    Book.java

--- LAB day 3/Book.java
+++ LAB day 3/Book.java
@@ -31,5 +31,17 @@
             System.out.println("Author: " + author);
             System.out.println("Price: " + price);
         }
+
+        public static void main(String[] args) {
+        Book book1 = new Book();
+        book1.display();
+
+        Book book2 = new Book("1984", "Argho");
+        book2.display();
+
+        Book book3 = new Book("Bangla", "Munni", 300);
+        book3.display();
     }
+}
+
 
